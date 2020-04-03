402e175132834fce0d6f32d29b8178ceb87393ca
Luke Frauhigher <lukefrogger@gmail.com>
2020-04-02 22:17:40 -0600

    finalizations

--- src/templates/skill.js
+++ src/templates/skill.js
@@ -21,18 +21,29 @@ export default ({ data: {skill} }) => (
       <BackgroundAngle color="255,255,255" slash="forward" />
       <div className="page-wrapper ">
         <SectionTitle category={skill.category} title={skill.pageTitle} color="blue" />
-        <h5 className="bottom-margin-40" dangerouslySetInnerHTML={{__html: skill.description}} />
+        { skill.description && 
+          <h5 className="bottom-margin-40" dangerouslySetInnerHTML={{__html: skill.description}} />
+        }
         {
           skill.body.map(section => {
-            if(section.imageCenter) {
+            if(!section.image) {
+              return (
+                <div className="flex-grid bottom-margin-40">
+                  <div className="flex4">
+                    <div dangerouslySetInnerHTML={{__html: section.textNode.childMarkdownRemark.html}} />
+                  </div>
+                </div>
+              )
+            }
+            else if(section.imageCenter) {
               return (
                 <>
-                  <div className="flex-grid bottom-margin-20">
+                  <div className="flex-grid bottom-margin-40">
                     <div className="flex1 picture">
                         <Img fluid={section.image.fluid} objectFit="cover" />
                     </div>
                   </div>
-                  <div className="flex-grid bottom-margin-20">
+                  <div className="flex-grid bottom-margin-40">
                     <div className="flex1">
                       <div dangerouslySetInnerHTML={{__html: section.textNode.childMarkdownRemark.html}} />
                     </div>
@@ -40,21 +51,21 @@ export default ({ data: {skill} }) => (
                 </>
               )
             }
-            else if(section.imageRight) {
-              return (
-                <div className="flex-grid bottom-margin-20">
-                  <div className="flex1 picture">
-                      <Img fluid={section.image.fluid} objectFit="cover" />
-                  </div>
-                  <div className="flex4">
-                    <div dangerouslySetInnerHTML={{__html: section.textNode.childMarkdownRemark.html}} />
-                  </div>
-                </div>
-              )
-            }
             else if(section.imageLeft) {
               return (
-                <div className="flex-grid bottom-margin-20">
+                <div className="flex-grid bottom-margin-40">
+                  <div className="flex1 picture">
+                      <Img fluid={section.image.fluid} objectFit="cover" />
+                  </div>
+                  <div className="flex4">
+                    <div dangerouslySetInnerHTML={{__html: section.textNode.childMarkdownRemark.html}} />
+                  </div>
+                </div>
+              )
+            }
+            else if(section.imageRight) {
+              return (
+                <div className="flex-grid bottom-margin-40">
                   <div className="flex4">
                     <div dangerouslySetInnerHTML={{__html: section.textNode.childMarkdownRemark.html}} />
                   </div>
@@ -64,6 +75,9 @@ export default ({ data: {skill} }) => (
                 </div>
               )
             }
+            return (
+              <div>There's missing content here.</div>
+            )
           })
         }
       </div>
